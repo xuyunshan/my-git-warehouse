1271a9553fe7429b2aef1eb62316d75005fca187
xuyunshan <xuyunshan@suresource.com.cn>
2020-07-04 15:27:37 +0800

    ss

---
+++ myvue/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+    <div class="box">
+      <div class="p">123</div>
+    </div>
+  </div>
+</template>
+
+<style lang='scss'>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.box{
+  background: #ccc;
+  .p{
+    color:red;
+  }
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
