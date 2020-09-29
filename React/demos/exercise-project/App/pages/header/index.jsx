98d732e7e91d507942e5b1b94e29f19a24320a47
xuyunshan <18333240521@163.com>
2020-09-29 10:59:50 +0800

    删掉了一些不用的项目

--- React/demos/exercise-project/App/pages/header/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './index.scss'
-const Header = ()=>{
-    return (
-        <h3 className="app-header">人员管理系统</h3>
-    )
-}
-
-export default Header
--- React/react-hook/demo01/README.md
+++
@@ -1,9 +0,0 @@
-# React hook 入门
-
-使用 webpack-dev-server 启动。
-
-webpack 命令进行打包。
-
-## 在线浏览
-
-[react-hook-useState](https://pschina.github.io/react-hook/demo01/)
